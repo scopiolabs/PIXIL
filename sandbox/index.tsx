694e48d968a070e2c65406d90f7dc9ccdfea56eb
Tomer Atar Kraken <tomera@scopiolabs.com>
2023-09-18 11:32:50 +0300

    fix: change to cjs

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log({ React, ReactDOM })
+
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
